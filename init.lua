9ff87b874d0669fa21324d7ec6322e1c62f56141
geekifan <i@ifan.dev>
2025-08-05 16:49:03 +0800

    reorganize require statements

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.basic")
+require("config.keybindings")
 require("config.lazy")
 require("config.icons")
-require("config.keybindings")
 require("config.plugins.lualine")
 require("config.plugins.dashboard")
 vim.cmd("colorscheme dayfox")
