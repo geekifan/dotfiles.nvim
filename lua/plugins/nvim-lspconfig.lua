5fe5a525233b1b0e5fe5141789c4f29c2559d9d6
geekifan <i@ifan.dev>
2025-08-09 19:03:11 +0800

    add ensure installed lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -10,6 +10,17 @@ return {
                 }
             }
         }},
-        { "mason-org/mason-lspconfig.nvim", opts = {} },
+        {
+            "mason-org/mason-lspconfig.nvim",
+            opts = {
+                ensure_installed = {
+                    -- python
+                    "pyright",
+                    "pylint",
+                    "debugpy",
+                    "yapf",
+                }
+            }
+        }
     },
 }
