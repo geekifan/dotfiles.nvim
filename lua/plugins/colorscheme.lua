40a7bc9311c1992e3028850e406a15d841ed34d4
geekifan <i@ifan.dev>
2025-08-10 12:36:23 +0800

    put colorschemes into one file

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "EdenEast/nightfox.nvim",
+        init = function()
+            vim.cmd("colorscheme dayfox")
+        end,
+    },
+    { "folke/tokyonight.nvim" },
+    { "projekt0n/github-nvim-theme" },
+    { "catppuccin/nvim" },
+    { "sainnhe/everforest" },
+}
