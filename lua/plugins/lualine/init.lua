7fa95922522438d73f54300de0042197e3f47258
geekifan <i@ifan.dev>
2025-08-10 09:47:03 +0800

    update lualine disabled_filetypes

--- lua/plugins/lualine/init.lua
+++ lua/plugins/lualine/init.lua
@@ -11,7 +11,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         options = {
-            disabled_filetypes = {"NvimTree", "Packer", "vista", "vista_markdown"},
+            disabled_filetypes = {"neo-tree", "Packer", "vista", "vista_markdown"},
             theme = "auto",
             component_separators = {
                 left = "",
