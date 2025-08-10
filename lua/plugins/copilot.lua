acc28bbea4b0dd1b11fe4d3b5e3ef5b9f8cf3118
geekifan <i@ifan.dev>
2025-08-10 14:49:36 +0800

    add copilot-lsp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,14 +1,46 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-        suggestion = {
-            enabled = true,
-            auto_trigger = true,
-            keymap = {
-                accept = "<Tab>",
-            }
-        },
-    }
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                keymap = {
+                    accept = "<Tab>",
+                }
+            },
+        }
+    },
+    {
+        "copilotlsp-nvim/copilot-lsp",
+        init = function()
+            vim.g.copilot_nes_debounce = 500
+            vim.lsp.enable("copilot_ls")
+            vim.keymap.set("n", "<tab>", function()
+                local bufnr = vim.api.nvim_get_current_buf()
+                local state = vim.b[bufnr].nes_state
+                if state then
+                    -- Try to jump to the start of the suggestion edit.
+                    -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+                    local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+                        or (
+                            require("copilot-lsp.nes").apply_pending_nes()
+                            and require("copilot-lsp.nes").walk_cursor_end_edit()
+                        )
+                    return nil
+                else
+                    -- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
+                    return "<C-i>"
+                end
+            end, { desc = "Accept Copilot NES suggestion", expr = true })
+            -- Clear copilot suggestion with Esc if visible, otherwise preserve default Esc behavior
+            vim.keymap.set("n", "<esc>", function()
+                if not require("copilot-lsp.nes").clear() then
+                    -- fallback to other functionality
+                end
+            end, { desc = "Clear Copilot suggestion or fallback" })
+        end,
+    },
 }
