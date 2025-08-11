5e3678d5ea09b5af9788656073a5bfc62db6e309
geekifan <i@ifan.dev>
2025-08-11 14:03:20 +0800

    add autosave

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,39 @@
+return {
+    "Pocco81/auto-save.nvim",
+    opts = {
+        -- avoid autosave for claudecode diff buffers (https://github.com/coder/claudecode.nvim)
+        -- ... other options
+        condition = function(buf)
+            local fn = vim.fn
+            local utils = require("auto-save.utils.data")
+        
+            -- First check the default conditions
+            if not (fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {})) then
+                return false
+            end
+        
+            -- Exclude claudecode diff buffers by buffer name patterns
+            local bufname = vim.api.nvim_buf_get_name(buf)
+            if bufname:match("%(proposed%)") or
+                bufname:match("%(NEW FILE %- proposed%)") or
+                bufname:match("%(New%)") then
+                return false
+            end
+        
+            -- Exclude by buffer variables (claudecode sets these)
+            if vim.b[buf].claudecode_diff_tab_name or
+                vim.b[buf].claudecode_diff_new_win or
+                vim.b[buf].claudecode_diff_target_win then
+                return false
+            end
+        
+            -- Exclude by buffer type (claudecode diff buffers use "acwrite")
+            local buftype = fn.getbufvar(buf, "&buftype")
+            if buftype == "acwrite" then
+                return false
+            end
+        
+            return true -- Safe to auto-save
+        end,
+    },
+}
