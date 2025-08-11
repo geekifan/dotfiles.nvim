b70e034bb75e61d12b81a3e1f5aaf2461c053774
geekifan <i@ifan.dev>
2025-08-11 20:13:07 +0800

    implement filesystem poller in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -26,20 +26,28 @@ return {
         use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
         -- instead of relying on nvim autocmd events.
     },
-    -- init = function()
-    --     vim.loop.new_timer():start(1000, 1000, vim.schedule_wrap(function()
-    --         local manager = require("neo-tree.sources.manager")
-    --         local state   = manager.get_state("filesystem")
-    --         -- 当前激活的 source 也是 filesystem 才刷新
-    --         if state
-    --            and state.winid
-    --            and vim.api.nvim_win_is_valid(state.winid)
-    --            and manager.get_active_source(state.winid) == "filesystem"
-    --         then
-    --             require("neo-tree.sources.filesystem.commands").refresh(state)
-    --         end
-    --     end))
-    -- end,
+    init = function()
+        vim.loop.new_timer():start(1000, 1000, vim.schedule_wrap(function()
+            local manager = require("neo-tree.sources.manager")
+            local state   = manager.get_state("filesystem")
+            if not state or not state.winid then
+                return
+            end
+            local bufnr = vim.api.nvim_win_get_buf(state.winid)
+            local source_status, source_name = pcall(vim.api.nvim_buf_get_var, bufnr, "neo_tree_source")
+            if not source_status then
+                return
+            end
+            -- refresh the filesystem if the current source is filesystem
+            if state
+               and state.winid
+               and vim.api.nvim_win_is_valid(state.winid)
+               and source_name == "filesystem"
+            then
+                require("neo-tree.sources.filesystem.commands").refresh(state)
+            end
+        end))
+    end,
     opts = {
         sources = {
             "filesystem",
