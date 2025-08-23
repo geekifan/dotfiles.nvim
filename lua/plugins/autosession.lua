9d5522e5dd6f956024b6b5f8fda4a7d101f220e3
geekifan <i@ifan.dev>
2025-08-23 17:53:21 +0800

    add autosession

---
+++ lua/plugins/autosession.lua
@@ -0,0 +1,11 @@
+return {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+        cwd_change_handling = true,
+    },
+}
